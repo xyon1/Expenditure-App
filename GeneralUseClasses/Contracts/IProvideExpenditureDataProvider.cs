108846fabefc31132021ac655a28632c3c7972a7
Benedict Quinn <xyon_zodiac@hotmail.co.uk>
2017-10-11 17:54:28 +0100

    Add IProvideExpenditureDataProvider contract
    This will support data provision service if inherited by a factory

---
+++ GeneralUseClasses/Contracts/IProvideExpenditureDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneralUseClasses.Services;
+
+namespace GeneralUseClasses.Contracts
+{
+    public interface IProvideExpenditureDataProvider
+    {
+        IProvideExpenditureData GetExpenditureDataProvider();
+    }
+}
