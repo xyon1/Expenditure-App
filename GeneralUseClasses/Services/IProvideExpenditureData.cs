9c6dd28e7d3cc2a24a7a5154d1f4604fd0920c7d
Benedict Quinn <xyon_zodiac@hotmail.co.uk>
2017-10-11 17:58:23 +0100

    Add IProvideExpenditure class to support data provision

---
+++ GeneralUseClasses/Services/IProvideExpenditureData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneralUseClasses.Contracts;
+
+namespace GeneralUseClasses.Services
+{
+    public interface IProvideExpenditureData
+    {
+         IEnumerable<string> GetDominantTags();
+
+        IEnumerable<string> GetAssociatedTags();
+
+        IEnumerable<string> GetPeople();
+    }
+}
