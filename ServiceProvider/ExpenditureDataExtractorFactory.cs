38b567ddfd6d1249744540815852e131f2c98e9b
Benedict Quinn <xyon_zodiac@hotmail.co.uk>
2017-10-11 18:01:40 +0100

    Adding ExpenditureDataExtractorFactory class to support data provision

---
+++ ServiceProvider/ExpenditureDataExtractorFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneralUseClasses.Contracts;
+using GeneralUseClasses.Services;
+using XmlClasses;
+
+namespace ServiceProvider
+{
+    public class ExpenditureDataExtractorFactory : IProvideExpenditureDataProvider
+    {
+        private IProvideExpenditureData recorder;
+
+        public ExpenditureDataExtractorFactory()
+        {
+            recorder = new ExtractExpenditureDataXml();
+        }
+
+        public IProvideExpenditureData GetExpenditureDataProvider()
+        {
+            return recorder;
+        }
+    }
+}
