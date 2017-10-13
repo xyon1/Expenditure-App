33f8c2d29e6e68cb28ac79ab159af9d8605dd629
Benedict Quinn <xyon_zodiac@hotmail.co.uk>
2017-10-13 12:23:57 +0100

    Add custom dialog to give user option of selecting folder or using default to store xml data on first setup

---
+++ ExpenditureAppWPFView/Dialogs/FolderSelectionDecisionDialog.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExpenditureAppWPF.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for FolderSelectionDecisionDialog.xaml
+    /// </summary>
+    public partial class FolderSelectionDecisionDialog : Window
+    {
+        private bool chooseFolder = false;
+        private bool useDefaultLocation = false;
+
+        public FolderSelectionDecisionDialog()
+        {
+            InitializeComponent();
+            chooseFolderBtn.Click += (s, e) => OnChooseFolderClicked();
+            useDefaultBtn.Click += (s, e) => OnUseDefaultClicked();
+            closeApplicationBtn.Click += (s, e) => OnCloseApplicationClicked();
+        }
+
+        public FolderSelectionDecisionDialogResult Show()
+        {
+            ShowDialog();
+            
+            if (chooseFolder)
+            {
+                return FolderSelectionDecisionDialogResult.ChooseFolder;
+            }
+            else if (useDefaultLocation)
+            {
+                return FolderSelectionDecisionDialogResult.UseDefaultLocation;
+            }
+            else
+            {
+                return FolderSelectionDecisionDialogResult.CloseApplication;
+            }
+        }
+
+        private void OnChooseFolderClicked()
+        {
+            chooseFolder = true;
+            Close();
+        }
+
+        private void OnUseDefaultClicked()
+        {
+            useDefaultLocation = true;
+            Close();
+        }
+
+        private void OnCloseApplicationClicked()
+        {
+            Close();
+        }
+    }
+}
---
+++ ExpenditureAppWPFView/Dialogs/FolderSelectionDecisionDialogResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenditureAppWPF.Dialogs
+{
+    public enum FolderSelectionDecisionDialogResult
+    {
+        ChooseFolder = 0,
+        UseDefaultLocation = 1,
+        CloseApplication = 2
+    }
+}
