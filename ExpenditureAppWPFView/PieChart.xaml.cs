7410ed74f889ab6998a6567254b1d0fb042da3c3
Benedict Quinn <xyon_zodiac@hotmail.co.uk>
2017-10-13 17:37:02 +0100

    Add Window for displaying pie chart of expenditure

---
+++ ExpenditureAppWPFView/PieChart.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ExpenditureAppWPF
+{
+    /// <summary>
+    /// Interaction logic for PieChart.xaml
+    /// </summary>
+    public partial class PieChart : Window
+    {
+        public PieChart()
+        {
+            InitializeComponent();
+            //SetupChart()
+        }
+
+        //private void SetupChart(Chart chart)
+        //{
+        //    Series expenditureSeries = new Series("Expenditure");
+
+        //    expenditureSeries.ChartType = SeriesChartType.Pie;
+        //    expenditureSeries.ChartArea = "pieChart";
+        //    expenditureSeries.Legend = "Legend1";
+        //    chart.Series.Add(expenditureSeries);
+        //    chart.Series["Expenditure"].Label = "#VALX #VALY";
+        //    chart.Series["Expenditure"].SetCustomProperty("PieStartAngle", "270");
+        //}
+    }
+}
